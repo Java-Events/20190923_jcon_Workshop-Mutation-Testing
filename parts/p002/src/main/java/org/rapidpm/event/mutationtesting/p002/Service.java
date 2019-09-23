7ce82860ffa246a89b505d2b89ee92fd0727e9d3
Mantas Kasperavicius <mkasperavicius@i-doit.com>
2019-09-23 14:28:19 +0200

    add new method

--- parts/p002/src/main/java/org/rapidpm/event/mutationtesting/p002/Service.java
+++ parts/p002/src/main/java/org/rapidpm/event/mutationtesting/p002/Service.java
@@ -9,9 +9,21 @@ public class Service {
 
   public int add(int a , int b) {
     if (a < 2) {
-      return (a + b) * - 1;
+      return (a + b) * - 1 ;
     } else {
       return a + b;
     }
+  }
+
+  public int tuWas(int a, int b, int c){
+    int ergebnis = 0;
+    for(int i=0; i<c; i++){
+      if(a<b){
+        ergebnis +=  a * i - b;
+      } else {
+        ergebnis +=  a + b + i;
+      }
+    }
+    return ergebnis;
   }
 }
