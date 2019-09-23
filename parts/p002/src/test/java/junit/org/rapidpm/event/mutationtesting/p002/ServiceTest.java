2b42da0767ee2bc8d0eff2679ef287ce9c87ccd3
Christian Esken <Christian.Esken@trivago.com>
2019-09-23 14:23:04 +0200

    unit tests for p002

--- parts/p002/src/test/java/junit/org/rapidpm/event/mutationtesting/p002/ServiceTest.java
+++ parts/p002/src/test/java/junit/org/rapidpm/event/mutationtesting/p002/ServiceTest.java
@@ -1,5 +1,6 @@
 package junit.org.rapidpm.event.mutationtesting.p002;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.rapidpm.event.mutationtesting.p002.Service;
 
@@ -8,17 +9,40 @@ import org.rapidpm.event.mutationtesting.p002.Service;
  */
 class ServiceTest {
 
+
+    @Test
+    void test000() {
+        final Service service = new Service();
+        Assertions.assertEquals( 0, service.add(0,0));
+    }
+
   @Test
-  void test001() {
-    //start here...
-    final Service service = new Service();
-
-
-
+  void test001a() {
+        final Service service = new Service();
+        Assertions.assertEquals( -1, service.add(1,0));
   }
+
+    @Test
+    void test001b() {
+        final Service service = new Service();
+        Assertions.assertEquals( -5, service.add(1,4));
+    }
 
   @Test
   void test002() {
-    final Service service = new Service();
+      final Service service = new Service();
+      Assertions.assertEquals(6, service.add(2, 4));
   }
+
+    @Test
+    void test003() {
+        final Service service = new Service();
+        Assertions.assertEquals(7, service.add(3, 4), "3+4");
+    }
+
+    @Test
+    void testMinus1() {
+        final Service service = new Service();
+        Assertions.assertEquals(1, service.add(-1, 0), "3+4");
+    }
 }
