dd8bccbc97b75a36a96f3493eac5d2631cdcab8d
Serban Iordache <serban.iordache@scoop-software.de>
2019-09-23 14:28:55 +0200

    change add

--- parts/p002/src/main/java/org/rapidpm/event/mutationtesting/p002/Service.java
+++ parts/p002/src/main/java/org/rapidpm/event/mutationtesting/p002/Service.java
@@ -9,9 +9,9 @@ public class Service {
 
   public int add(int a , int b) {
     if (a < 2) {
-      return (a + b) * - 1;
+      return (a - b) * - 1;
     } else {
-      return a + b;
+      return -a + b;
     }
   }
 }
