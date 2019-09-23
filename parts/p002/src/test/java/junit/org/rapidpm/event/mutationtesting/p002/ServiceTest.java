d279d190350cf3c547488ac95df2a5fe2207452d
Adam Wehner <awehner@ophardt.com>
2019-09-23 15:01:45 +0200

    tests implemented

--- parts/p002/src/test/java/junit/org/rapidpm/event/mutationtesting/p002/ServiceTest.java
+++ parts/p002/src/test/java/junit/org/rapidpm/event/mutationtesting/p002/ServiceTest.java
@@ -2,22 +2,23 @@ package junit.org.rapidpm.event.mutationtesting.p002;
 
 import org.junit.jupiter.api.Test;
 import org.rapidpm.event.mutationtesting.p002.Service;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Create the best test coverage you can for this
  */
 class ServiceTest {
 
-  @Test
-  void test001() {
-    final Service service = new Service();
-    assertEquals(-2, service.add(Integer.MAX_VALUE, Integer.MAX_VALUE));
-  }
+	@Test
+	void test001() {
+		final Service service = new Service();
+		assertEquals(1, service.add(-3, -2));
+	}
 
-  @Test
-  void test002() {
-    final Service service = new Service();
-    assertEquals(1, service.add(Integer.MIN_VALUE, Integer.MAX_VALUE));
-  }
+	@Test
+	void test002() {
+		final Service service = new Service();
+		assertEquals(-2, service.add(3, 1));
+	}
 }
